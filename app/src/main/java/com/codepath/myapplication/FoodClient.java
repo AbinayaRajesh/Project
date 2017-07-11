36f3b68f022d447d74c2da9d7e2a383cea5bc0d8
Emily Lauren Roth <emilylroth@fb.com>
2017-07-11 14:47:01 -0700

    food client

--- app/src/main/java/com/codepath/myapplication/FoodClient.java
+++ app/src/main/java/com/codepath/myapplication/FoodClient.java
@@ -1,5 +1,68 @@
-package com.codepath.myapplication; /**
+package com.codepath.myapplication;
+
+import com.github.scribejava.core.builder.api.DefaultApi10a;
+import com.github.scribejava.core.model.OAuth1RequestToken;
+
+/**
  * Created by emilylroth on 7/11/17.
  */
-public class FoodClient {
+
+
+public class FoodClient extends DefaultApi10a {
+
+    private static final String AUTHORIZE_URL = "https://api.twitter.com/oauth/authorize?oauth_token=%s";
+    private static final String REQUEST_TOKEN_RESOURCE = "api.twitter.com/oauth/request_token";
+    private static final String ACCESS_TOKEN_RESOURCE = "api.twitter.com/oauth/access_token";
+
+    protected FoodClient() {
+    }
+
+    private static class InstanceHolder {
+        private static final FoodClient INSTANCE = new FoodClient();
+    }
+
+    public static FoodClient instance() {
+        return InstanceHolder.INSTANCE;
+    }
+
+    @Override
+    public String getAccessTokenEndpoint() {
+        return "https://" + ACCESS_TOKEN_RESOURCE;
+    }
+
+    @Override
+    public String getRequestTokenEndpoint() {
+        return "https://" + REQUEST_TOKEN_RESOURCE;
+    }
+
+    @Override
+    public String getAuthorizationUrl(OAuth1RequestToken requestToken) {
+        return String.format(AUTHORIZE_URL, requestToken.getToken());
+    }
+
+    /**
+     * Twitter 'friendlier' authorization endpoint for OAuth.
+     * <p>
+     * Uses SSL.
+     */
+    public static class Authenticate extends FoodClient {
+
+        private static final String AUTHENTICATE_URL = "https://api.twitter.com/oauth/authenticate?oauth_token=%s";
+
+        private Authenticate() {
+        }
+
+        private static class InstanceHolder {
+            private static final Authenticate INSTANCE = new Authenticate();
+        }
+
+        public static Authenticate instance() {
+            return InstanceHolder.INSTANCE;
+        }
+
+        @Override
+        public String getAuthorizationUrl(OAuth1RequestToken requestToken) {
+            return String.format(AUTHENTICATE_URL, requestToken.getToken());
+        }
+    }
 }
