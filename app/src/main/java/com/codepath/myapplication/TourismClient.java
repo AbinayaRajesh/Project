ab7ff4e3301d60a3c07cd45d494c518afe56cb19
Abinaya Rajesh <arajesh@fb.com>
2017-07-11 14:08:57 -0700

    Yelp Client

---
+++ app/src/main/java/com/codepath/myapplication/TourismClient.java
@@ -0,0 +1,38 @@
+package com.codepath.myapplication;
+
+import android.content.Context;
+
+import com.codepath.oauth.OAuthBaseClient;
+import com.github.scribejava.core.builder.api.BaseApi;
+
+
+/**
+ * Created by arajesh on 7/11/17.
+ */
+
+public class TourismClient extends OAuthBaseClient {
+
+    public static final BaseApi REST_API_INSTANCE = YelpApi.instance(); // Change this
+    public static final String REST_URL = "https://api.twitter.com/1.1"; // Change this, base API URL
+    public static final String REST_CONSUMER_KEY = "MPmXyQTYraTgpMdfqJ6iaFBeX";       // Change this
+    public static final String REST_CONSUMER_SECRET = "pmbYL4Vpj0zb5F6gZhrVDunYqqkaL3IjUWHPeAEOq3kxXAizeE"; // Change this
+
+    // Landing page to indicate the OAuth flow worked in case Chrome for Android 25+ blocks navigation back to the app.
+    public static final String FALLBACK_URL = "https://codepath.github.io/android-rest-client-template/success.html";
+
+    // See https://developer.chrome.com/multidevice/android/intents
+    public static final String REST_CALLBACK_URL_TEMPLATE = "intent://%s#Intent;action=android.intent.action.VIEW;scheme=%s;package=%s;S.browser_fallback_url=%s;end";
+
+    public TourismClient(Context context) {
+        super(context, REST_API_INSTANCE,
+                REST_URL,
+                REST_CONSUMER_KEY,
+                REST_CONSUMER_SECRET,
+                String.format(REST_CALLBACK_URL_TEMPLATE, context.getString(R.string.intent_host),
+                        context.getString(R.string.intent_scheme), context.getPackageName(), FALLBACK_URL));
+    }
+    // CHANGE THIS
+    // DEFINE METHODS for different API endpoints here
+
+
+}
