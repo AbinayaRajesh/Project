9feb6d767220dad652017e9907271d6177e9b969
Eyob Tefera <eyobtefera@fb.com>
2017-07-12 11:18:29 -0700

    Added stuff

--- app/src/main/java/com/codepath/myapplication/Event/Event.java
+++ app/src/main/java/com/codepath/myapplication/Event/Event.java
@@ -1,16 +1,13 @@
 package com.codepath.myapplication.Event;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
  * Created by eyobtefera on 7/11/17.
  */
-
-public class Event implements Parcelable {
+@org.parceler.Parcel
+public class Event  {
     public String eventName;
     public String eventDescription;
     public String eventUrl;
@@ -30,37 +27,6 @@ public class Event implements Parcelable {
         return event;
     }
 
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(this.eventName);
-        dest.writeString(this.eventDescription);
-        dest.writeString(this.eventUrl);
-        dest.writeValue(this.eventCapacity);
-    }
-
-    protected Event(Parcel in) {
-        this.eventName = in.readString();
-        this.eventDescription = in.readString();
-        this.eventUrl = in.readString();
-        this.eventCapacity = (Integer) in.readValue(Integer.class.getClassLoader());
-    }
-
-    public static final Creator<Event> CREATOR = new Creator<Event>() {
-        @Override
-        public Event createFromParcel(Parcel source) {
-            return new Event(source);
-        }
-
-        @Override
-        public Event[] newArray(int size) {
-            return new Event[size];
-        }
-    };
 
     public String getEventName() {
         return eventName;
