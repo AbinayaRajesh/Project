2b5a10391623d164caff747f8b1d9f2a409f87fa
Eyob Tefera <eyobtefera@fb.com>
2017-07-13 15:22:30 -0700

    Updated eventadapter class

--- app/src/main/java/com/codepath/myapplication/Event/EventAdapter.java
+++ app/src/main/java/com/codepath/myapplication/Event/EventAdapter.java
@@ -1,54 +1,78 @@
 package com.codepath.myapplication.Event;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.codepath.myapplication.R;
-import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
 
 /**
  * Created by eyobtefera on 7/11/17.
  */
 
-public class EventAdapter extends ArrayAdapter<Event> {
+public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ViewHolder> {
 
     private List<Event> mEvents;
-    EventBriteClient client;
     Context context;
     //pass in the Tweets array in the constructor
-    public EventAdapter(Context context, ArrayList<Event> events) {
-            super(context, 0, events);
-        }
-    public View getView(int position, View convertView, ViewGroup parent) {
-        // Get the data item for this position
-        Event event = getItem(position);
-        // Check if an existing view is being reused, otherwise inflate the view
-        if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_event, parent, false);
-        }
-
-        // Lookup view for data population
-        TextView tvEventName = (TextView) convertView.findViewById(R.id.tvEventName);
-        TextView tvEventCapacity = (TextView) convertView.findViewById(R.id.tvEventCapacity);
-        TextView tvEventDescription = (TextView) convertView.findViewById(R.id.tvEventDescription);
-        ImageView ivEventImage = (ImageView) convertView.findViewById(R.id.ivEventImage);
-        // Populate the data into the template view using the data object
-        tvEventName.setText(event.eventName);
-        tvEventCapacity.setText(event.eventCapacity);
-        tvEventDescription.setText(event.eventDescription);
-        Picasso.with(context).load(event.getEventUrl()).into(ivEventImage);
-
-        // Return the completed view to render on screen
-        return convertView;
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View eventView = inflater.inflate(R.layout.item_event, parent, false);
+        ViewHolder viewHolder = new ViewHolder(eventView);
+        return viewHolder;
     }
 
+    //bind the values based on the position of the element
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        //get the data according to position
+        Event event = mEvents.get(position);
+        //populate the views according to this data
+        holder.tvEventName.setText(event.eventName);
+        holder.tvEventCapacity.setText(event.eventCapacity);
+        holder.tvEventDescription.setText(event.eventDescription);
+
+        Glide.with(context).
+                load(event.getEventUrl()).
+                bitmapTransform(new RoundedCornersTransformation(context, 15, 0)).
+                into(holder.ivEventImage);
+    }
+    @Override
+    public int getItemCount() {
+        return mEvents.size();
+    }
+    //create ViewHolder class
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public ImageView ivEventImage;
+        public TextView tvEventName;
+        public TextView tvEventCapacity;
+        public TextView tvEventDescription;
+        public RelativeLayout layout;
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            // perform findViewById lookups
+            ivEventImage = (ImageView) itemView.findViewById(R.id.ivEventImage);
+            tvEventName = (TextView) itemView.findViewById(R.id.tvEventName);
+            tvEventCapacity = (TextView) itemView.findViewById(R.id.tvEventCapacity);
+            tvEventDescription = (TextView) itemView.findViewById(R.id.tvEventDescription);
+            layout = (RelativeLayout) itemView.findViewById(R.id.detailView);
+
+        }
+    }
 }
