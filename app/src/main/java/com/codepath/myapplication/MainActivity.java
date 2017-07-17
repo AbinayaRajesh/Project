a4e7f4548af50dfddff50ca8164f8b9cdb9d537a
Emily Lauren Roth <emilylroth@fb.com>
2017-07-17 13:56:23 -0700

    Final

--- app/src/main/java/com/codepath/myapplication/MainActivity.java
+++ app/src/main/java/com/codepath/myapplication/MainActivity.java
@@ -2,13 +2,11 @@ package com.codepath.myapplication;
 
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -54,16 +52,16 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        eventButton = (Button) findViewById(R.id.bttnEvent);
+        //eventButton = (Button) findViewById(R.id.bttnEvent);
         context = this;
-        eventButton.setOnClickListener(new View.OnClickListener() {
+     /*   eventButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(context, FoodActivity.class);
                 i.putExtra("query", "food");
                 startActivity(i);
-            }
-        });
+
+        });*/
 
 
 
