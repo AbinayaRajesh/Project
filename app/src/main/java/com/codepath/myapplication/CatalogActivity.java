a9e6623a3a744a61b2162dfe9dcd63dc07f1f84b
Abinaya Rajesh <arajesh@fb.com>
2017-07-24 13:38:43 -0700

    database works

--- app/src/main/java/com/codepath/myapplication/CatalogActivity.java
+++ app/src/main/java/com/codepath/myapplication/CatalogActivity.java
@@ -26,10 +26,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
-
-import com.codepath.myapplication.data.PetContract.PetEntry;
-import com.codepath.myapplication.data.PetDbHelper;
+import com.codepath.myapplication.data.EventContract.EventEntry;
+import com.codepath.myapplication.data.EventDbHelper;
 
 /**
  * Displays list of pets that were entered and stored in the app.
@@ -37,7 +37,7 @@ import com.codepath.myapplication.data.PetDbHelper;
 public class CatalogActivity extends AppCompatActivity {
 
     /** Database helper that will provide us access to the database */
-    private PetDbHelper mDbHelper;
+    private EventDbHelper mDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,7 +56,7 @@ public class CatalogActivity extends AppCompatActivity {
 
         // To access our database, we instantiate our subclass of SQLiteOpenHelper
         // and pass the context, which is the current activity.
-        mDbHelper = new PetDbHelper(this);
+        mDbHelper = new EventDbHelper(this);
     }
 
     @Override
@@ -76,15 +76,17 @@ public class CatalogActivity extends AppCompatActivity {
         // Define a projection that specifies which columns from the database
         // you will actually use after this query.
         String[] projection = {
-                PetEntry._ID,
-                PetEntry.COLUMN_PET_NAME,
-                PetEntry.COLUMN_PET_BREED,
-                PetEntry.COLUMN_PET_GENDER,
-                PetEntry.COLUMN_PET_WEIGHT };
+                EventEntry._ID,
+                EventEntry.COLUMN_EVENT_NAME,
+                EventEntry.COLUMN_EVENT_DESCRIPTION,
+                EventEntry.COLUMN_EVENT_URL,
+                EventEntry.COLUMN_EVENT_VENUE,
+                EventEntry.COLUMN_EVENT_START_TIME,
+                EventEntry.COLUMN_EVENT_STOP_TIME};
 
         // Perform a query on the pets table
         Cursor cursor = db.query(
-                PetEntry.TABLE_NAME,   // The table to query
+                EventEntry.TABLE_NAME,   // The table to query
                 projection,            // The columns to return
                 null,                  // The columns for the WHERE clause
                 null,                  // The values for the WHERE clause
@@ -103,18 +105,22 @@ public class CatalogActivity extends AppCompatActivity {
             // In the while loop below, iterate through the rows of the cursor and display
             // the information from each column in this order.
             displayView.setText("The pets table contains " + cursor.getCount() + " pets.\n\n");
-            displayView.append(PetEntry._ID + " - " +
-                    PetEntry.COLUMN_PET_NAME + " - " +
-                    PetEntry.COLUMN_PET_BREED + " - " +
-                    PetEntry.COLUMN_PET_GENDER + " - " +
-                    PetEntry.COLUMN_PET_WEIGHT + "\n");
+            displayView.append(EventEntry._ID + " - " +
+                    EventEntry.COLUMN_EVENT_NAME + " - " +
+                    EventEntry.COLUMN_EVENT_DESCRIPTION + " - " +
+                    EventEntry.COLUMN_EVENT_URL + " - " +
+                    EventEntry.COLUMN_EVENT_VENUE + " - " +
+                    EventEntry.COLUMN_EVENT_START_TIME + " - " +
+                    EventEntry.COLUMN_EVENT_STOP_TIME + "\n");
 
             // Figure out the index of each column
-            int idColumnIndex = cursor.getColumnIndex(PetEntry._ID);
-            int nameColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_NAME);
-            int breedColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_BREED);
-            int genderColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_GENDER);
-            int weightColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_WEIGHT);
+            int idColumnIndex = cursor.getColumnIndex(EventEntry._ID);
+            int nameColumnIndex = cursor.getColumnIndex(EventEntry.COLUMN_EVENT_NAME);
+            int descriptionColumnIndex = cursor.getColumnIndex(EventEntry.COLUMN_EVENT_DESCRIPTION);
+            int urlColumnIndex = cursor.getColumnIndex(EventEntry.COLUMN_EVENT_URL);
+            int venueColumnIndex = cursor.getColumnIndex(EventEntry.COLUMN_EVENT_VENUE);
+            int startColumnIndex = cursor.getColumnIndex(EventEntry.COLUMN_EVENT_START_TIME);
+            int stopColumnIndex = cursor.getColumnIndex(EventEntry.COLUMN_EVENT_STOP_TIME);
 
             // Iterate through all the returned rows in the cursor
             while (cursor.moveToNext()) {
@@ -122,15 +128,19 @@ public class CatalogActivity extends AppCompatActivity {
                 // at the current row the cursor is on.
                 int currentID = cursor.getInt(idColumnIndex);
                 String currentName = cursor.getString(nameColumnIndex);
-                String currentBreed = cursor.getString(breedColumnIndex);
-                int currentGender = cursor.getInt(genderColumnIndex);
-                int currentWeight = cursor.getInt(weightColumnIndex);
+                String currentDescription = cursor.getString(descriptionColumnIndex);
+                String currentUrl = cursor.getString(urlColumnIndex);
+                String currentVenue = cursor.getString(venueColumnIndex);
+                String currentStart = cursor.getString(startColumnIndex);
+                String currentStop = cursor.getString(stopColumnIndex);
                 // Display the values from each column of the current row in the cursor in the TextView
                 displayView.append(("\n" + currentID + " - " +
                         currentName + " - " +
-                        currentBreed + " - " +
-                        currentGender + " - " +
-                        currentWeight));
+                        currentDescription + " - " +
+                        currentUrl + " - " +
+                        currentVenue + " - " +
+                        currentStart + " - " +
+                        currentStop));
             }
         } finally {
             // Always close the cursor when you're done reading from it. This releases all its
@@ -142,27 +152,27 @@ public class CatalogActivity extends AppCompatActivity {
     /**
      * Helper method to insert hardcoded pet data into the database. For debugging purposes only.
      */
-    private void insertPet() {
-        // Gets the database in write mode
-        SQLiteDatabase db = mDbHelper.getWritableDatabase();
-
-        // Create a ContentValues object where column names are the keys,
-        // and Toto's pet attributes are the values.
-        ContentValues values = new ContentValues();
-        values.put(PetEntry.COLUMN_PET_NAME, "Toto");
-        values.put(PetEntry.COLUMN_PET_BREED, "Terrier");
-        values.put(PetEntry.COLUMN_PET_GENDER, PetEntry.GENDER_MALE);
-        values.put(PetEntry.COLUMN_PET_WEIGHT, 7);
-
-        // Insert a new row for Toto in the database, returning the ID of that new row.
-        // The first argument for db.insert() is the pets table name.
-        // The second argument provides the name of a column in which the framework
-        // can insert NULL in the event that the ContentValues is empty (if
-        // this is set to "null", then the framework will not insert a row when
-        // there are no values).
-        // The third argument is the ContentValues object containing the info for Toto.
-        long newRowId = db.insert(PetEntry.TABLE_NAME, null, values);
-    }
+//    private void insertPet() {
+//        // Gets the database in write mode
+//        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+//
+//        // Create a ContentValues object where column names are the keys,
+//        // and Toto's pet attributes are the values.
+//        ContentValues values = new ContentValues();
+//        values.put(EventEntry.COLUMN_EVENT_NAME, "Toto");
+//        values.put(EventEntry.COLUMN_EVENT_DESCRIPTION, "Terrier");
+//        values.put(EventEntry.COLUMN_EVENT_URL, EventEntry.GENDER_MALE);
+//        values.put(EventEntry.COLUMN_EVENT_VENUE, 7);
+//
+//        // Insert a new row for Toto in the database, returning the ID of that new row.
+//        // The first argument for db.insert() is the pets table name.
+//        // The second argument provides the name of a column in which the framework
+//        // can insert NULL in the event that the ContentValues is empty (if
+//        // this is set to "null", then the framework will not insert a row when
+//        // there are no values).
+//        // The third argument is the ContentValues object containing the info for Toto.
+//        long newRowId = db.insert(EventEntry.TABLE_NAME, null, values);
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -178,7 +188,8 @@ public class CatalogActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             // Respond to a click on the "Insert dummy data" menu option
             case R.id.action_insert_dummy_data:
-                insertPet();
+                // insertPet();
+                insertEvent();
                 displayDatabaseInfo();
                 return true;
             // Respond to a click on the "Delete all entries" menu option
@@ -188,4 +199,48 @@ public class CatalogActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+
+    private void insertEvent() {
+
+        // Read from input fields
+        // Use trim to eliminate leading or trailing white space
+        // mNameEditText.getText().toString().trim();
+        String nameString = "NAME";
+        String descriptionString = "DES";
+        String urlString = "URL";
+        String venueString = "VENUE";
+        String startString = "START";
+        String stopString = "STOP";
+
+        // Create database helper
+        EventDbHelper mDbHelper = new EventDbHelper(this);
+
+        // Gets the database in write mode
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        // Create a ContentValues object where column names are the keys,
+        // and pet attributes from the editor are the values.
+        ContentValues values = new ContentValues();
+        values.put(EventEntry.COLUMN_EVENT_NAME, nameString);
+        values.put(EventEntry.COLUMN_EVENT_DESCRIPTION, descriptionString);
+        values.put(EventEntry.COLUMN_EVENT_URL, urlString);
+        values.put(EventEntry.COLUMN_EVENT_VENUE, venueString);
+        values.put(EventEntry.COLUMN_EVENT_START_TIME, startString);
+        values.put(EventEntry.COLUMN_EVENT_STOP_TIME, stopString);
+
+        // Insert a new row for pet in the database, returning the ID of that new row.
+        long newRowId = db.insert(EventEntry.TABLE_NAME, null, values);
+
+        // Show a toast message depending on whether or not the insertion was successful
+        if (newRowId == -1) {
+            // If the row ID is -1, then there was an error with insertion.
+            Toast.makeText(this, "Error with saving pet", Toast.LENGTH_SHORT).show();
+        } else {
+            // Otherwise, the insertion was successful and we can display a toast with the row ID.
+            Toast.makeText(this, "Pet saved with row id: " + newRowId, Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
--- app/src/main/java/com/codepath/myapplication/EditorActivity.java
+++ app/src/main/java/com/codepath/myapplication/EditorActivity.java
@@ -20,18 +20,14 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import com.codepath.myapplication.data.PetContract.PetEntry;
-import com.codepath.myapplication.data.PetDbHelper;
+import com.codepath.myapplication.data.EventContract.EventEntry;
+import com.codepath.myapplication.data.EventDbHelper;
 
 /**
  * Allows user to create a new pet or edit an existing one.
@@ -50,12 +46,7 @@ public class EditorActivity extends AppCompatActivity {
     /** EditText field to enter the pet's gender */
     private Spinner mGenderSpinner;
 
-    /**
-     * Gender of the pet. The possible valid values are in the PetContract.java file:
-     * {@link PetEntry#GENDER_UNKNOWN}, {@link PetEntry#GENDER_MALE}, or
-     * {@link PetEntry#GENDER_FEMALE}.
-     */
-    private int mGender = PetEntry.GENDER_UNKNOWN;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,85 +59,85 @@ public class EditorActivity extends AppCompatActivity {
         mWeightEditText = (EditText) findViewById(R.id.edit_pet_weight);
         mGenderSpinner = (Spinner) findViewById(R.id.spinner_gender);
 
-        setupSpinner();
+        // setupSpinner();
     }
 
-    /**
-     * Setup the dropdown spinner that allows the user to select the gender of the pet.
-     */
-    private void setupSpinner() {
-        // Create adapter for spinner. The list options are from the String array it will use
-        // the spinner will use the default layout
-        ArrayAdapter genderSpinnerAdapter = ArrayAdapter.createFromResource(this,
-                R.array.array_gender_options, android.R.layout.simple_spinner_item);
+//    /**
+//     * Setup the dropdown spinner that allows the user to select the gender of the pet.
+//     */
+//    private void setupSpinner() {
+//        // Create adapter for spinner. The list options are from the String array it will use
+//        // the spinner will use the default layout
+//        ArrayAdapter genderSpinnerAdapter = ArrayAdapter.createFromResource(this,
+//                R.array.array_gender_options, android.R.layout.simple_spinner_item);
+//
+//        // Specify dropdown layout style - simple list view with 1 item per line
+//        genderSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+//
+//        // Apply the adapter to the spinner
+//        mGenderSpinner.setAdapter(genderSpinnerAdapter);
+//
+//        // Set the integer mSelected to the constant values
+//        mGenderSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                String selection = (String) parent.getItemAtPosition(position);
+//                if (!TextUtils.isEmpty(selection)) {
+//                    if (selection.equals(getString(R.string.gender_male))) {
+//                        mGender = EventEntry.GENDER_MALE;
+//                    } else if (selection.equals(getString(R.string.gender_female))) {
+//                        mGender = EventEntry.GENDER_FEMALE;
+//                    } else {
+//                        mGender = EventEntry.GENDER_UNKNOWN;
+//                    }
+//                }
+//            }
+//
+//            // Because AdapterView is an abstract class, onNothingSelected must be defined
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//                mGender = EventEntry.GENDER_UNKNOWN;
+//            }
+//        });
+//    }
 
-        // Specify dropdown layout style - simple list view with 1 item per line
-        genderSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-
-        // Apply the adapter to the spinner
-        mGenderSpinner.setAdapter(genderSpinnerAdapter);
-
-        // Set the integer mSelected to the constant values
-        mGenderSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selection = (String) parent.getItemAtPosition(position);
-                if (!TextUtils.isEmpty(selection)) {
-                    if (selection.equals(getString(R.string.gender_male))) {
-                        mGender = PetEntry.GENDER_MALE;
-                    } else if (selection.equals(getString(R.string.gender_female))) {
-                        mGender = PetEntry.GENDER_FEMALE;
-                    } else {
-                        mGender = PetEntry.GENDER_UNKNOWN;
-                    }
-                }
-            }
-
-            // Because AdapterView is an abstract class, onNothingSelected must be defined
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                mGender = PetEntry.GENDER_UNKNOWN;
-            }
-        });
-    }
-
-    /**
-     * Get user input from editor and save new pet into database.
-     */
-    private void insertPet() {
-        // Read from input fields
-        // Use trim to eliminate leading or trailing white space
-        String nameString = mNameEditText.getText().toString().trim();
-        String breedString = mBreedEditText.getText().toString().trim();
-        String weightString = mWeightEditText.getText().toString().trim();
-        int weight = Integer.parseInt(weightString);
-
-        // Create database helper
-        PetDbHelper mDbHelper = new PetDbHelper(this);
-
-        // Gets the database in write mode
-        SQLiteDatabase db = mDbHelper.getWritableDatabase();
-
-        // Create a ContentValues object where column names are the keys,
-        // and pet attributes from the editor are the values.
-        ContentValues values = new ContentValues();
-        values.put(PetEntry.COLUMN_PET_NAME, nameString);
-        values.put(PetEntry.COLUMN_PET_BREED, breedString);
-        values.put(PetEntry.COLUMN_PET_GENDER, mGender);
-        values.put(PetEntry.COLUMN_PET_WEIGHT, weight);
-
-        // Insert a new row for pet in the database, returning the ID of that new row.
-        long newRowId = db.insert(PetEntry.TABLE_NAME, null, values);
-
-        // Show a toast message depending on whether or not the insertion was successful
-        if (newRowId == -1) {
-            // If the row ID is -1, then there was an error with insertion.
-            Toast.makeText(this, "Error with saving pet", Toast.LENGTH_SHORT).show();
-        } else {
-            // Otherwise, the insertion was successful and we can display a toast with the row ID.
-            Toast.makeText(this, "Pet saved with row id: " + newRowId, Toast.LENGTH_SHORT).show();
-        }
-    }
+//    /**
+//     * Get user input from editor and save new pet into database.
+//     */
+//    private void insertPet() {
+//        // Read from input fields
+//        // Use trim to eliminate leading or trailing white space
+//        String nameString = mNameEditText.getText().toString().trim();
+//        String breedString = mBreedEditText.getText().toString().trim();
+//        String weightString = mWeightEditText.getText().toString().trim();
+//        int weight = Integer.parseInt(weightString);
+//
+//        // Create database helper
+//        EventDbHelper mDbHelper = new EventDbHelper(this);
+//
+//        // Gets the database in write mode
+//        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+//
+//        // Create a ContentValues object where column names are the keys,
+//        // and pet attributes from the editor are the values.
+//        ContentValues values = new ContentValues();
+//        values.put(EventEntry.COLUMN_EVENT_NAME, nameString);
+//        values.put(EventEntry.COLUMN_EVENT_DESCRIPTION, breedString);
+//        values.put(EventEntry.COLUMN_EVENT_URL, mGender);
+//        values.put(EventEntry.COLUMN_EVENT_VENUE, weight);
+//
+//        // Insert a new row for pet in the database, returning the ID of that new row.
+//        long newRowId = db.insert(EventEntry.TABLE_NAME, null, values);
+//
+//        // Show a toast message depending on whether or not the insertion was successful
+//        if (newRowId == -1) {
+//            // If the row ID is -1, then there was an error with insertion.
+//            Toast.makeText(this, "Error with saving pet", Toast.LENGTH_SHORT).show();
+//        } else {
+//            // Otherwise, the insertion was successful and we can display a toast with the row ID.
+//            Toast.makeText(this, "Pet saved with row id: " + newRowId, Toast.LENGTH_SHORT).show();
+//        }
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -163,7 +154,8 @@ public class EditorActivity extends AppCompatActivity {
             // Respond to a click on the "Save" menu option
             case R.id.action_save:
                 // Save pet to database
-                insertPet();
+                // insertPet();
+                insertEvent();
                 // Exit activity
                 finish();
                 return true;
@@ -178,5 +170,46 @@ public class EditorActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void insertEvent() {
+
+        // Read from input fields
+        // Use trim to eliminate leading or trailing white space
+        // mNameEditText.getText().toString().trim();
+        String nameString = "NAME";
+        String descriptionString = "DES";
+        String urlString = "URL";
+        String venueString = "VENUE";
+        String startString = "START";
+        String stopString = "STOP";
+
+        // Create database helper
+        EventDbHelper mDbHelper = new EventDbHelper(this);
+
+        // Gets the database in write mode
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        // Create a ContentValues object where column names are the keys,
+        // and pet attributes from the editor are the values.
+        ContentValues values = new ContentValues();
+        values.put(EventEntry.COLUMN_EVENT_NAME, nameString);
+        values.put(EventEntry.COLUMN_EVENT_DESCRIPTION, descriptionString);
+        values.put(EventEntry.COLUMN_EVENT_URL, urlString);
+        values.put(EventEntry.COLUMN_EVENT_VENUE, venueString);
+        values.put(EventEntry.COLUMN_EVENT_START_TIME, startString);
+        values.put(EventEntry.COLUMN_EVENT_STOP_TIME, stopString);
+
+        // Insert a new row for pet in the database, returning the ID of that new row.
+        long newRowId = db.insert(EventEntry.TABLE_NAME, null, values);
+
+        // Show a toast message depending on whether or not the insertion was successful
+        if (newRowId == -1) {
+            // If the row ID is -1, then there was an error with insertion.
+            Toast.makeText(this, "Error with saving pet", Toast.LENGTH_SHORT).show();
+        } else {
+            // Otherwise, the insertion was successful and we can display a toast with the row ID.
+            Toast.makeText(this, "Pet saved with row id: " + newRowId, Toast.LENGTH_SHORT).show();
+        }
     }
 }
